fe8c3f362a673aa1b2467a531ef96118739a836f
kkata <redprin900@yahoo.co.jp>
2018-08-22 18:08:58 +0900

    model

---
+++ spec/models/note_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+RSpec.describe Note, type: :model do
+  before do
+    # このファイルの全テストで使用するテストデータをセットアップする
+    @user = User.create(
+      first_name: "Joe",
+      last_name:  "Tester",
+      email:      "joetester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
+    @project = @user.projects.create(
+      name: "Test Project"
+    )
+  end
+
+  # ユーザー、プロジェクト、メッセージがあれば有効な状態であること
+  it "is valid with a user, project, and message" do
+    note = Note.new(
+      message: "This is a samle note.",
+      user: @user,
+      project: @project,
+    )
+    expect(note).to be_valid
+  end
+
+  # メッセージがなければ無効な状態であること
+  it "is invalid without a message" do
+    note = Note.new(message: nil)
+    note.valid?
+    expect(note.errors[:message]).to include("can't be blank")
+  end
+
+  # 検索結果を検証するスペック
+  # 文字列に一致するメッセージを検索する
+  describe "search message for a term" do
+    before do
+      # 検索機能の全テストに関連する追加のテストデータをセットアップする
+      @note1 = @project.notes.create(
+        message: "This is the first note.",
+        user: @user,
+      )
+      @note2 = @project.notes.create(
+        message: "This is the second note.",
+        user: @user,
+      )
+      @note3 = @project.notes.create(
+        message: "First, preheat the oven.",
+        user: @user,
+      )
+    end
+
+    # 検索用の example が並ぶ ...
+    # 一致するデータが見つかるとき
+    context "when a match is found" do
+      # 一致する場合の example が並ぶ ...
+      # 検索文字列に一致するメモを返すこと
+      it "returns notes that match the search term" do
+        expect(Note.search("first")).to include(@note1, @note3)
+      end
+    end
+    # 一致するデータが1件も見つからないとき
+    context "when no match is found" do
+      # 一致しない場合の example が並ぶ ...
+      # 空のコレクションを返すこと
+      it "returns an empty collection when no results are found" do
+        expect(Note.search("message")).to be_empty
+      end
+    end
+  end
+end
